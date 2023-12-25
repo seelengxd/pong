16e97c6ee77c219c4dde65c47d8768863b817665
see leng <seelengxd@gmail.com>
2023-12-25 17:38:29 +0800

    feat: make ball bounce

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,45 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default class Demo extends Phaser.Scene {
+  xSpeed = 200;
+  ySpeed = 200;
+
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image("logo", "assets/pong-logo.png");
+    this.load.image("ball", "assets/ball.svg");
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    const logo = this.add.image(400, 300, "logo").setScale(0.5);
+    const ball = this.physics.add
+      .sprite(400, 300, "ball")
+      .setScale(0.2)
+      .refreshBody();
+    ball.setCollideWorldBounds(true);
+    ball.body.onWorldBounds = true;
+    ball.setVelocity(this.xSpeed, this.ySpeed);
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
+    this.physics.world.on("worldbounds", (body, up, down, left, right) => {
+      if (down) {
+        body.velocity.y = -this.ySpeed;
+      }
+      if (up) {
+        body.velocity.y = this.ySpeed;
+      }
+      if (left) {
+        body.velocity.x = -this.xSpeed;
+      }
+      if (right) {
+        body.velocity.x = this.xSpeed;
+      }
+
+      if (left || right) {
+        body.velocity.x = -body.velocity.x;
+      }
     });
   }
 }
